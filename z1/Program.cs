f3fd0fdb2f80cc40d251cab3084ceed4f30d4210
evgeniy <shtorm20300@gmail.com>
2022-12-20 23:04:42 +0600

    больше 0

---
+++ z1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+Console.Write("Введите количество чисел: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[number];
+int[] arrayNum = NumberOfNumbers(array);
+PrintArray(arrayNum);
+AboveZero(arrayNum);
+
+int[] NumberOfNumbers(int[] arr)
+{   
+    int[] arrayNew = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write((i+1) + "-ое: ");
+        arrayNew[i] = int.Parse(Console.ReadLine()!);
+    }
+    return arrayNew;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int AboveZero(int[] arrN)
+{
+    int count = 0;
+    for (int i = 0; i < arrN.Length; i++)
+    {
+        if (arrN[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Колличество элементов > 0 = " + count);
+    return count;
+}
